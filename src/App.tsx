fb9e8ff96b3f8a7b90a8e668b25e262a2dc39e1b
Sergey Dobrovolsky <misterdobrovolsky2017@gmail.com>
2024-12-09 22:11:57 +0200

    added mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Button from '@mui/material/Button';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
+
 function App() {
-  return (<h2>Start project</h2>)
+  return (
+    <>
+    <Button variant="contained">Test Button</Button>
+    <Stack >
+      <Pagination count={10} color="primary" />
+    </Stack>
+    </>
+  )
 }
 
 export default App
