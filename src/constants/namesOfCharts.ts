fe7897889d113061587c76ed77984bd5a3448fd2
Serhii <69472491+Serhii-Dobrovolskyi@users.noreply.github.com>
2025-01-29 23:48:10 +0200

    Delete src/constants/namesOfCharts.ts

--- src/constants/namesOfCharts.ts
+++
@@ -1,8 +0,0 @@
-const NAMES_OF_CHARTS = ['Maximum Temperature',
-   'Minimum Temperature',
-   'Mean Temperature',
-   'Maximum Apparent Temperature',
-   'Minimum Apparent Temperature',
-   'Mean Apparent Temperature']
-
-export default NAMES_OF_CHARTS
